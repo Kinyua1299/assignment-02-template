b02f65dc344bd598c4201536682222b78fe6fa24
kinyua1299 <kingkinyua664@gmail.com>
2025-09-08 14:02:36 +0300

    test 3

--- src/__tests__/test-challenge-3.js
+++ src/__tests__/test-challenge-3.js
@@ -4,3 +4,14 @@ import { celsiusToFahrenheit } from '../assignment'
 it('challenge - 3 : my celsiusToFahrenheit function can convert an array of temperatures', async () => {
     expect(celsiusToFahrenheit([51.21, 23, 0, -55, 100])).toStrictEqual([124, 73, 32, -67, 212])
 })
+export function celsiusToFahrenheit(celsiusArray) {
+  let fahrenheitArray = []
+
+  for (let i = 0; i < celsiusArray.length; i++) {
+    let f = (celsiusArray[i] * 9/5) + 32
+    // round so it matches the expected whole numbers
+    fahrenheitArray.push(Math.round(f))
+  }
+
+  return fahrenheitArray
+}
